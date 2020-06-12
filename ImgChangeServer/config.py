7a20ae734ee930c2191f4c4e0e468c3c0940f168
WarrentLee <1065791740@qq.com>
2020-06-13 00:16:05 +0800

    Update configuration

--- ImgChangeServer/config.py
+++ ImgChangeServer/config.py
@@ -2,7 +2,8 @@ import os
 
 
 class Config:
-    DATASET_DIRECTORY = os.path.dirname(os.getcwd())+"/dataset"
-    DATABASE_DIRECTORY = os.path.dirname(os.getcwd())+"/database/data.db"
-    AnimeGAN_MODEL_DIRECTORY = os.path.abspath(os.path.dirname(os.getcwd())) + "/workbench/AnimeGAN/checkpoint/model"
-    WarpGAN_MODEL_DIRECTORY = os.path.abspath(os.path.dirname(os.getcwd())) + "/workbench/WarpGAN/pretrained/warpgan_pretrained"
+    SERVER_DIRECTORY = os.path.dirname(os.getcwd())
+    DATASET_DIRECTORY = SERVER_DIRECTORY+"/dataset"
+    DATABASE_DIRECTORY = SERVER_DIRECTORY+"/database/data.db"
+    AnimeGAN_MODEL_DIRECTORY = os.path.abspath(SERVER_DIRECTORY) + "/workbench/AnimeGAN/checkpoint/model"
+    WarpGAN_MODEL_DIRECTORY = os.path.abspath(SERVER_DIRECTORY) + "/workbench/WarpGAN/pretrained/warpgan_pretrained"
